38a1020721598a868a8700269eceb50c2c606994
vladimir <52894331+vladi2909@users.noreply.github.com>
2022-02-19 18:40:17 +0300

    add links

--- script.js
+++ script.js
@@ -1,9 +1,25 @@
-$(".search__icon").click(function () {
-    $(".search-toggle").animate({
-        width: 'toggle'
+$(document).ready(function() {
+    $(".search__icon").click(function () {
+        $(".search-toggle").animate({
+            width: 'toggle'
+        });
+    
+        $(".icon-open").toggle();
+        $(".icon-close").toggle();
+        $(".search-form").val('');
     });
 
-    $(".icon-open").toggle();
-    $(".icon-close").toggle();
-    $(".search-form").val('');
+    $(".navigation__link").click(function(e) {
+        const target = e.target;
+
+        $(".navigation__item").each(function() {
+            $(this).removeClass("navigation__item--active");
+        });
+
+        $(target.parentElement).addClass("navigation__item--active");
+
+        
+    });
+    
+
 });
